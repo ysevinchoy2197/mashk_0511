a0516974022cfe5f3be7a094aba71131cffbd006
ysevinchoy2197 <ysevinchoy2197@gmail.com>
2025-11-05 18:04:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#1-misol
+for i in range(1, 51):
+    print(i)
+    if i == 42:
+        break
+
+
+#2-misol
+for i in range(1, 21):
+    if i % 2 == 2:
+        print(i)
+
+#3-misol
+for i in range(1, 31):
+    if i % 5 == 0:
+        print(i)
+
+#4-misol
+for i in range(1, 16):
+    if i % 3 != 0:
+        print(i)
+
+#5-misol
+for i in range(1, 21):
+    if i % 25 == 0:
+        print("diapazonda 25 soni bor ")
+    else:
+        print("diapazonda 25 soni yo'q! ")
+        break
